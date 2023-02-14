55f7a454b1e675a5b29d30e3ec44a57082688292
Edgardo Vaca <edgardovac2298@gmail.com>
2023-02-14 09:08:25 -0500

    change to execute success the pipeline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,3 @@
 export const environment = {
-  production: false,
-  config: {
-    tenant: 'b5e244bd-c492-495b-8b10-61bfd453e423',
-    clientId: '6c8eb53b-8318-4ed7-886d-9f75f409b34f',
-    authority: 'https://login.microsoftonline.com/b5e244bd-c492-495b-8b10-61bfd453e423',
-    redirectUri: `http://localhost:4200/home`,
-    postLogoutRedirectUri: `http://localhost:4200/login`,
-    endpoints: {
-      API: 'http://localhost:3000/dev',
-      FILE_API: 'http://localhost:9000'
-    },
-    cacheLocation: 'localStorage',
-  },
-  environment: 'local'
+    production: true,
 };
